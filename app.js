0bc36e84b5957dc3e1da6d97a488ae7dc97b74b5
Kap Thang <73055456+thangk@users.noreply.github.com>
2022-08-01 02:52:30 -0400

    added cors and dotenv

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
+const cors = require('cors')
+
+require('dotenv').config();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 // app.get('/', async (req, res, next) => {
 //   res.send({message: "Hello World"});
 // })
+
+// added by Kap
+// we'll need to change this origin back to netlify's url when we go live which I'll set it up in ENV file
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN
+  })
+)
+
+
+
 app.use('/', require('./routes/watches'));
 app.use('/', require('./routes/carts'));
 app.use('/', require('./routes/customers'));
