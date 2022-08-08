1011fb2813f62af77a3dc51d7394292190a4dbe0
EthanJeffery <ethanjeffery190@gmail.com>
2022-08-08 16:55:26 -0400

    Delete cartWatch by watchId and cartId (one or all that match)

--- routes/cart-watch.js
+++ routes/cart-watch.js
@@ -70,4 +70,35 @@ router.delete('/cart-watches/:id', async function(req, res, next)  {
     }
 })
 
+router.delete('/cart-watches', async function(req, res, next)  {
+    try {
+        let deletedWatches= {}
+        if(req.body.deleteAll) {
+            deletedWatches = await prisma.cartWatch.deleteMany({
+                where: {
+                    cartId: Number(req.body.cartId),
+                    watchId: Number(req.body.watchId),
+                }
+            })
+        } else {
+            watchToDelete = await prisma.cartWatch.findFirst({
+                where: {
+                    cartId: Number(req.body.cartId),
+                    watchId: Number(req.body.watchId),
+                }
+            })
+            deletedWatches = await prisma.cartWatch.delete({
+                where: {
+                    id: watchToDelete.id,
+                }
+            })
+        }
+        res.json(deletedWatches);
+    } catch (err) {
+        console.error('Error while getting cartWatches', err.message);
+        next(err);
+    }
+})
+
+
 module.exports = router;
